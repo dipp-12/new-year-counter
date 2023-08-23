4b8442d7f50be3c1e2ab0ae488aaeb13877e9580
Muhammad Nadhif Nashrullah <nadhifn1261@gmail.com>
2023-08-23 09:06:39 +0700

    add days and modified counter based on current year

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 const countdown = () => {
-    const dueDate = new Date("Jan 1, 2023 00:00:00").getTime()
-    const now =  new Date().getTime()
+    const currentYear = new Date().getFullYear()
+    const nextYear = currentYear + 1
+    const dueDate = new Date(`Jan 1, ${nextYear} 00:00:00`).getTime()
+    const now = new Date().getTime()
     const gap = dueDate - now
 
     const second = 1000
     const minute = second * 60
     const hour = minute * 60
+    const day = hour * 24
 
-    const getHour = Math.floor(gap / hour)
+    const getDay = Math.floor(gap / day)
+    const getHour = Math.floor((gap % day) / hour)
     const getMinute = Math.floor((gap % hour) / minute)
     const getSecond = Math.floor((gap % minute) / second)
-    
+
+    document.querySelector('.year-text').innerText = nextYear
+    document.querySelector('.day-text').innerText = getDay
     document.querySelector('.hour-text').innerText = getHour
     document.querySelector('.minute-text').innerText = getMinute
     document.querySelector('.second-text').innerText = getSecond
@@ -23,7 +29,7 @@ setInterval(countdown, 100)
 const a = document.queryCommandValue('.second-text').innerText
 
 const test = () => {
-    if(a == '5'){
+    if (a == '5') {
         console.log('true')
     }
 }
